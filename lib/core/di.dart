1791f2b3ce98add4de1748c27f04075cc4727b79
Mohamed Saied <mohamedsaidsalah0111@gmail.com>
2025-02-04 17:59:55 +0200

    added dependency injection to caching manger

--- lib/core/di.dart
+++ lib/core/di.dart
@@ -1,15 +1,25 @@
+import 'package:cross_platform_product_listing_assessment/core/cache_manager.dart';
 import 'package:cross_platform_product_listing_assessment/data/repos/products_repo.dart';
 import 'package:get_it/get_it.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 final GetIt getIt = GetIt.instance;
 
 void setupGetIt() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
   // http client
   getIt.registerLazySingleton<http.Client>(() => http.Client());
 
   // products repo
   getIt.registerLazySingleton<ProductsRepo>(
       () => ProductsRepo(getIt<http.Client>()));
+
+  // shared prefs
+  getIt.registerLazySingleton<SharedPreferences>(() => prefs);
+
+  // cache manager
+  getIt.registerLazySingleton<CacheManager>(() => CacheManager(prefs));
 }
