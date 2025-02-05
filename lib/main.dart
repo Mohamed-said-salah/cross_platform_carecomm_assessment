e68bd1fe2178ec2ff11fe63a7e2107d15b1d87ac
Mohamed Saied <mohamedsaidsalah0111@gmail.com>
2025-02-05 14:54:28 +0200

    Created Theme Cubit to control the light and dark theming mode with keeping the light mode persistent on cache when app is terminated

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:cross_platform_product_listing_assessment/core/di.dart';
+import 'package:cross_platform_product_listing_assessment/logic/cubit/theme_cubit.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   setupGetIt();
+
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
 
   runApp(const MainApp());
 }
@@ -14,11 +25,30 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return BlocProvider(
+      create: (_) => ThemeCubit(),
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, mode) {
+          return MaterialApp(
+            title: "CareComm Task",
+            themeMode: mode,
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.dark(),
+            home: Scaffold(
+              body: Center(
+                child: BlocBuilder<ThemeCubit, ThemeMode>(
+                  builder: (context, themeMode) {
+                    return Switch(
+                      value: themeMode == ThemeMode.dark,
+                      onChanged: (value) =>
+                          context.read<ThemeCubit>().toggleTheme(),
+                    );
+                  },
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
