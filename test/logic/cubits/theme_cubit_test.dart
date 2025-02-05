322e5370850a975e15bc2e03a18618d8a4eebd02
Mohamed Saied <mohamedsaidsalah0111@gmail.com>
2025-02-05 18:54:02 +0200

    update: grouped tests for the theme cubit

--- test/logic/cubits/theme_cubit_test.dart
+++ test/logic/cubits/theme_cubit_test.dart
@@ -9,6 +9,7 @@ import 'theme_cubit_test.mocks.dart';
 @GenerateMocks([Storage])
 void main() {
   late MockStorage storage;
+  late ThemeCubit themeCubit;
 
   setUp(() {
     storage = MockStorage();
@@ -17,32 +18,41 @@ void main() {
     when(storage.write(any, any))
         .thenAnswer((_) async {}); // Mock writing state
     HydratedBloc.storage = storage;
+    themeCubit = ThemeCubit();
   });
 
-  test('initial state should be ThemeMode.light', () {
-    final themeCubit = ThemeCubit();
-    expect(themeCubit.state, ThemeMode.light);
+  tearDown(() {
+    themeCubit.close();
   });
 
-  test('toggleTheme should switch from light to dark and back', () {
-    final themeCubit = ThemeCubit();
-
-    themeCubit.toggleTheme();
-    expect(themeCubit.state, ThemeMode.dark);
-
-    themeCubit.toggleTheme();
-    expect(themeCubit.state, ThemeMode.light);
+  group('ThemeCubit Initialization', () {
+    test('should have an initial state of ThemeMode.light', () {
+      expect(themeCubit.state, ThemeMode.light);
+    });
   });
 
-  test('fromJson should return correct ThemeMode', () {
-    final themeCubit = ThemeCubit();
-    expect(themeCubit.fromJson({'theme': 1}), ThemeMode.light);
-    expect(themeCubit.fromJson({'theme': 2}), ThemeMode.dark);
+  group('ThemeCubit Toggle Functionality', () {
+    test('should toggle theme from light to dark', () {
+      themeCubit.toggleTheme();
+      expect(themeCubit.state, ThemeMode.dark);
+    });
+
+    test('should toggle theme from dark back to light', () {
+      themeCubit.toggleTheme();
+      themeCubit.toggleTheme();
+      expect(themeCubit.state, ThemeMode.light);
+    });
   });
 
-  test('toJson should return correct map', () {
-    final themeCubit = ThemeCubit();
-    expect(themeCubit.toJson(ThemeMode.light), {'theme': 1});
-    expect(themeCubit.toJson(ThemeMode.dark), {'theme': 2});
+  group('ThemeCubit Serialization', () {
+    test('fromJson should return correct ThemeMode', () {
+      expect(themeCubit.fromJson({'theme': 1}), ThemeMode.light);
+      expect(themeCubit.fromJson({'theme': 2}), ThemeMode.dark);
+    });
+
+    test('toJson should return correct map', () {
+      expect(themeCubit.toJson(ThemeMode.light), {'theme': 1});
+      expect(themeCubit.toJson(ThemeMode.dark), {'theme': 2});
+    });
   });
 }
