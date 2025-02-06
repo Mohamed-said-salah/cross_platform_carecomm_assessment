8ce679faadac5b98dd496f68be727ed14540ad57
Mohamed Saied <mohamedsaidsalah0111@gmail.com>
2025-02-06 15:28:46 +0200

    updated theme switcher to be responsive on tablet and desktop

--- lib/views/components/buttons/animated_theme_switcher.dart
+++ lib/views/components/buttons/animated_theme_switcher.dart
@@ -23,7 +23,7 @@ class AnimatedThemeSwitcher extends StatelessWidget {
           isDarkMode ? Icons.light_mode : Icons.dark_mode,
           key: ValueKey<bool>(
               isDarkMode), // Key helps AnimatedSwitcher know the state change
-          size: ResponsiveManager(context).responsiveValue(30.r, 80.r, 120.r),
+          size: ResponsiveManager(context).responsiveValue(30.r, 45.r, 55.r),
           color: isDarkMode
               ? Colors.amber
               : const Color.fromARGB(255, 81, 105, 117),
