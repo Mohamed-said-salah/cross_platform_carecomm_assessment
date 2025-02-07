a521ce1248d34a54c752b9d84937be125ead34ef
Mohamed Saied <mohamedsaidsalah0111@gmail.com>
2025-02-07 22:25:18 +0200

    added route to product detail screen

--- lib/core/router/app_router.gr.dart
+++ lib/core/router/app_router.gr.dart
@@ -10,10 +10,13 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:auto_route/auto_route.dart' as _i3;
-import 'package:cross_platform_product_listing_assessment/views/screens/home_screen.dart'
+import 'package:cross_platform_product_listing_assessment/data/models/product_model.dart'
+    as _i5;
+import 'package:cross_platform_product_listing_assessment/views/screens/home/home_screen.dart'
     as _i1;
 import 'package:cross_platform_product_listing_assessment/views/screens/product_details_screen.dart'
     as _i2;
+import 'package:flutter/material.dart' as _i4;
 
 /// generated route for
 /// [_i1.HomeScreen]
@@ -33,16 +36,37 @@ class HomeRoute extends _i3.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.ProductDetailsScreen]
-class ProductDetailsRoute extends _i3.PageRouteInfo<void> {
-  const ProductDetailsRoute({List<_i3.PageRouteInfo>? children})
-    : super(ProductDetailsRoute.name, initialChildren: children);
+class ProductDetailsRoute extends _i3.PageRouteInfo<ProductDetailsRouteArgs> {
+  ProductDetailsRoute({
+    _i4.Key? key,
+    required _i5.ProductModel product,
+    List<_i3.PageRouteInfo>? children,
+  }) : super(
+         ProductDetailsRoute.name,
+         args: ProductDetailsRouteArgs(key: key, product: product),
+         initialChildren: children,
+       );
 
   static const String name = 'ProductDetailsRoute';
 
   static _i3.PageInfo page = _i3.PageInfo(
     name,
     builder: (data) {
-      return const _i2.ProductDetailsScreen();
+      final args = data.argsAs<ProductDetailsRouteArgs>();
+      return _i2.ProductDetailsScreen(key: args.key, product: args.product);
     },
   );
+}
+
+class ProductDetailsRouteArgs {
+  const ProductDetailsRouteArgs({this.key, required this.product});
+
+  final _i4.Key? key;
+
+  final _i5.ProductModel product;
+
+  @override
+  String toString() {
+    return 'ProductDetailsRouteArgs{key: $key, product: $product}';
+  }
 }
