19e5860d97da5d98b809c928228f9fe9f7658185
Mohamed-said-salah <71166528+Mohamed-said-salah@users.noreply.github.com>
2025-02-08 15:15:14 +0000

    Deploying to gh-pages from @ Mohamed-said-salah/cross_platform_carecomm_assessment@02a9163b07c4c6bb786c43c477a86dd6bc4e2e83 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3220402923"
+    serviceWorkerVersion: "1008147069"
   }
 });
